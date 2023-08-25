1dbf42f7a988ce52cbb31fe7cecc002b82754663
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-24 22:09:07 -0700

    push main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	char *filepath = argv[1];
+	
+	FILE *file = fopen(filepath, "r");
+
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s \n", filepath);
+		exit(EXIT_FAILURE);
+	}
+
+	char *buffer_line = NULL;
+	size_t len = 0;
+	int line_number = 0;
+
+	while (getline(&buffer_line, &len, file) != -1)
+	{
+		line_number++;
+		//tokenize to separate string from integer
+		char *opcode = strtok(&buffer_line, "\t\n");
+		if (opcode == NULL || strlen(opcode) == 0)
+		{
+			continue;
+		}
+
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+	free(buffer_line);
+	fclose(file);
+	exit(EXIT_FAILURE);
+
+	return (0);
+}
